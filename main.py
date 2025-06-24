7a18c591efdf6b51e1bb26022f9245cc1a57bdcc
Carrou122 <sufiadzhar122@gmail.com>
2025-06-24 20:43:51 +0800

    B: added player with polygon

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     print('Starting Asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   
+    dt = 0
+    clock = pygame.time.Clock()
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  
-   
+        dt = (clock.tick(60)/1000)
+        player.draw(screen)
         pygame.display.flip()
+        
+    
     
 if __name__ == "__main__":
     
