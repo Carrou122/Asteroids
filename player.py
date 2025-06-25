fcfa927de4fc9cb7b5d7a2e6cb6a7dcf75d60942
Carrou122 <sufiadzhar122@gmail.com>
2025-06-25 18:55:55 +0800

    G:added shots

--- player.py
+++ player.py
@@ -37,4 +37,21 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+    
+    def shoot(self): 
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
+    
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
 
