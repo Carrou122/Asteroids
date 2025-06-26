ec0aa8758d58b2ce2740b043ffe9f85d53d18ca7
Carrou122 <sufiadzhar122@gmail.com>
2025-06-26 18:25:27 +0800

    H: controlled shots

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.rate_limit = 0
 
     # in the player class
     def triangle(self):
@@ -28,6 +29,7 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.rate_limit -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -39,12 +41,17 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+            
     
-    def shoot(self): 
+    def shoot(self):
+        if self.rate_limit > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.rate_limit = PLAYER_SHOOT_COOLDOWN
+        
+            
 
-    
 class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
@@ -54,4 +61,4 @@ class Shot(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
-
+        
